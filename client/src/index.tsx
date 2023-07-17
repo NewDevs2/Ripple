38252ba6f0d14c4f3daaf9ea9d9b6cc3dd0faa5c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-17 14:34:59 +0900

    service-worker를 설치하는 방법을 찾는 중

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -17,3 +17,15 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 푸시 알림 권한 요청
+if ("Notification" in window && "serviceWorker" in navigator) {
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      // 사용자가 푸시 알림 권한을 허용한 경우, Service Worker 등록
+      console.log("알림 권한 허용됨");
+    } else {
+      console.log("알림 권한 허용 안 됨");
+    }
+  });
+}
