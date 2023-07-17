7c8a679af8a70ac88f8530da728bea06b996656b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-17 12:41:07 +0900

    수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          let's research <code>Service Worker</code>.
+          <code>Ripple.</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
