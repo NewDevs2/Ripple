d44d7748278d104c9121fbf5b0c122325c2cbb7d
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-17 18:23:44 +0900

    알림 이름 변경

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = "Ripple";
 const urlsToCache = ["/"];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -16,18 +17,5 @@ self.addEventListener("fetch", (event) => {
       }
       return fetch(event.request);
     })
-  );
-});
-
-// 푸시 알림
-self.addEventListener("push", (event) => {
-  const options = {
-    body: "푸시 알림 테스트",
-    icon: "/favicon.ico",
-    // 추가적인 옵션들...
-  };
-
-  event.waitUntil(
-    self.registration.showNotification("푸시 알림 제목", options)
   );
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { send } from "process";
 
-
-const sendNotification = () => {
-  // Push Notification을 보내는 로직을 구현합니다.
-  // Service Worker 등록과 관련된 코드가 여기 들어갑니다.
-  if ("serviceWorker" in navigator && "PushManager" in window) {
-    // Service Worker 등록
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("Service Worker registered!");
-
-        // 푸시 알림 구독 요청
-        registration.pushManager
-          .subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: "YOUR_PUBLIC_VAPID_KEY",
-          })
-          .then((subscription) => {
-            console.log("Push notification subscribed:", subscription);
-          })
-          .catch((error) => {
-            console.error("Error subscribing to push notification:", error);
-          });
-      })
-      .catch((error) => {
-        console.error("Error registering Service Worker:", error);
-      });
+async function requestNotificationPermission() {
+  if ("Notification" in window) {
+    const permission = await Notification.requestPermission();
+    if (permission !== "granted") {
+      console.log("알림 권한이 요청이 거부되었습니다.");
+    }
   }
-};
+}
 
 const App: React.FC = () => {
+  useEffect(() => {
+    requestNotificationPermission();
+  }, []);
+  function handleNotificationClick() {
+    // 알림이 클릭되었을 때 수행할 동작을 정의합니다.
+    console.log("알림을 클릭했습니다.");
+  }
+
+  function sendNotification() {
+    if ("Notification" in window && Notification.permission === "granted") {
+      const notification = new Notification("Ripple", {
+        body: "누군가 회원님의 트랙을 좋아합니다.",
+        icon: "/favicon.ico", // 알림 아이콘 경로
+      });
+      notification.addEventListener("click", handleNotificationClick);
+    } else {
+      console.log("알림 권한이 없습니다. 알림을 허용해주세요.");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Ripple</code>.<button onClick={sendNotification}>푸시 알람 보내깅ㅎ</button>
+          <code>Ripple</code>.<br></br>
+          <button onClick={sendNotification}>푸시 알림 전송</button>
         </p>
       </header>
     </div>
