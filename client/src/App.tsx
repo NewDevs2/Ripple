68b38375531e55464b0d8e72503ab4c279c8d8f2
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-17 14:52:25 +0900

    CRA 템플릿 설정을 변경하여 service-worker를 설치함.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Ripple.</code>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
