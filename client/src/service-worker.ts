d9b0778cfb9b8bcb7b84564c2a6edaa1ba55d772
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-17 15:27:12 +0900

    service-worker.js를 추가해도 적용되지 않는다.

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -82,14 +82,3 @@ self.addEventListener("message", (event) => {
 
 // service-worker.js
 
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installed!");
-});
-
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated!");
-});
-
-self.addEventListener("fetch", (event) => {
-  console.log("Fetch intercepted:", event.request.url);
-});
