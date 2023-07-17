2fdc5c9d98f941483e2aaf01f42aba3478f7f045
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-17 14:49:30 +0900

    다시 시도해보기

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,6 +18,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
 
 // 푸시 알림 권한 요청
 if ("Notification" in window && "serviceWorker" in navigator) {
