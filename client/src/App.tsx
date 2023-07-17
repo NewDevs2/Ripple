61470abe081c0794ca23d1a6f328c678f1c59575
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-17 15:00:54 +0900

    service-worker는 production(배포)에서만 보이는 것 같다

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,14 +10,6 @@ function App() {
         <p>
           <code>Ripple</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
