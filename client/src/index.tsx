1456fe7c6301eae74ed14d4508a1ef0ad1fdc381
krapli441 <tlfqj1208@gmail.com>
2023-07-20 12:34:34 +0900

    파이어베이스 클라우드 메시징 기능을 사용해봤지만 별개의 앱 서버가 필요하다.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -52,7 +52,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
