79ba38d2210b750aa30ea2a67bda0538f2cfd1cd
krapli441 <tlfqj1208@gmail.com>
2023-07-20 14:29:22 +0900

    파이어베이스 Cloud function은 요금제를 사용해야 한다.

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -1,19 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// 푸시 알림 보내기 함수
+exports.sendPushNotification = functions.firestore
+  .document("notifications/{notificationId}")
+  .onCreate(async (snapshot: any, context: any) => {
+    const notificationData = snapshot.data();
+    const notificationTitle = notificationData.title;
+    const notificationBody = notificationData.body;
+    const token = notificationData.token;
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    const message = {
+      data: {
+        title: notificationTitle,
+        body: notificationBody,
+      },
+      token: token,
+    };
+
+    try {
+      const response = await admin.messaging().send(message);
+      console.log("푸시 알림이 성공적으로 보내졌습니다:", response);
+    } catch (error) {
+      console.error("푸시 알림 보내기 오류:", error);
+    }
+  });
