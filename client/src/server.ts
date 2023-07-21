73fded5ca1079ff1b058047b6c22f055d2daeeae
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-21 18:43:16 +0900

    manifest.json 경로를 수정해도 파일을 찾지 못해서 일단 제거 하고 진행

--- client/src/server.ts
+++ client/src/server.ts
@@ -1,13 +1,13 @@
 export { };
-
+const path = require('path');
 const express = require('express');
 const app = express();
-
 // 루트 경로에 대한 요청 처리
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.get('/', (req: any, res: any) => {
-  res.send('Hello, Express!');
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
-
+console.log(__dirname)
 // 서버 시작
 app.listen(3000, () => {
   console.log('서버 정상 실행');
