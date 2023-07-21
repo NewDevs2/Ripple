603349b3f9740e3f16ea19fbd51ce416fc78e0ff
krapli441 <tlfqj1208@gmail.com>
2023-07-21 12:37:39 +0900

    우선 geolocation으로 사용자 위치 정보 표시

--- client/src/geoloctaion.tsx
+++ client/src/geoloctaion.tsx
@@ -1,16 +1,46 @@
 // 리액트 라이브러리
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./geolocation.css";
 
 // 컴포넌트
 
 const Geolocation: React.FC = () => {
-  navigator.geolocation;
+  const [userLocation, setUserLocation] = useState<string | null>(null);
+
+  const getCurrentLocation = (): Promise<GeolocationPosition> =>
+    new Promise((resolve, reject) => {
+      if (!navigator.geolocation) {
+        reject(new Error("Geolocation is not available"));
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error)
+      );
+    });
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        const position = await getCurrentLocation();
+        const { latitude, longitude } = position.coords;
+        setUserLocation(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      } catch (error) {
+        setUserLocation("Unable to retrieve location");
+        console.error("Error retrieving location:", error);
+      }
+    };
+
+    fetchLocation();
+  }, []);
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <div className="userPosition"></div>
+          <div>
+            <h1>User Location</h1>
+            {userLocation ? <p>{userLocation}</p> : <p>Loading...</p>}
+          </div>
         </header>
       </div>
     </>
