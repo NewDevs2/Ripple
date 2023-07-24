51a6d803d0543b3ca7748c8fff13f7e58e8bfddd
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-24 14:08:53 +0900

    빌드 된 react파일 미들웨어 등록 -> 304에러

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 import { Express as expressMiddleware } from 'express';
 import * as path from 'path';
 import { StaticFileServeModule } from './static-file-serve/static-file-serve.module';
 import { SocketGateway } from './socket/socket.gateway';
 @Module({
   imports: [StaticFileServeModule],
-  controllers: [AppController],
-  providers: [AppService, SocketGateway],
+  controllers: [],
+  providers: [SocketGateway],
 })
 export class AppModule {}
--- server/src/static-file-serve/static-file-serve.module.ts
+++ server/src/static-file-serve/static-file-serve.module.ts
@@ -6,7 +6,14 @@ import * as path from 'path';
 export class StaticFileServeModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     // React 빌드된 정적 파일들의 경로를 설정합니다.
-    const staticPath = path.join(__dirname, '..', '..', '..', 'client', 'build');
-    consumer.apply(express.static(staticPath)).forRoutes('*'); // express.static을 사용하여 정적 파일 서빙
+    console.log(path.join(__dirname, '..', '..', '..', 'client', 'build'))
+    const buildPath = path.join(__dirname, '..', '..', '..', 'client', 'build');
+    const staticPaths = ['js', 'css', 'media'];
+    staticPaths.forEach((folder) => {
+        const folderPath = path.join(buildPath, 'static', folder);
+        consumer.apply(express.static(folderPath)).forRoutes('*');
+      });
+  
+    consumer.apply(express.static(buildPath)).forRoutes('*'); // express.static을 사용하여 정적 파일 서빙
   }
 }
