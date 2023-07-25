fe039e2648923c12da71bddd921c57f0921b1d8a
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-25 16:27:34 +0900

    미들 웨어를 빌드된 파일(최상위)만 등록 (요청 두번 감)

--- server/src/static-file-serve/static-file-serve.module.ts
+++ server/src/static-file-serve/static-file-serve.module.ts
@@ -8,11 +8,11 @@ export class StaticFileServeModule implements NestModule {
     // React 빌드된 정적 파일들의 경로를 설정합니다.
     console.log(path.join(__dirname, '..', '..', '..', 'client', 'build'))
     const buildPath = path.join(__dirname, '..', '..', '..', 'client', 'build');
-    const staticPaths = ['js', 'css', 'media'];
-    staticPaths.forEach((folder) => {
-        const folderPath = path.join(buildPath, 'static', folder);
-        consumer.apply(express.static(folderPath)).forRoutes('*');
-      });
+    // const staticPaths = ['js', 'css', 'media'];
+    // staticPaths.forEach((folder) => {
+    //     const folderPath = path.join(buildPath, 'static', folder);
+    //     consumer.apply(express.static(folderPath)).forRoutes('*');
+    //   });
   
     consumer.apply(express.static(buildPath)).forRoutes('*'); // express.static을 사용하여 정적 파일 서빙
   }
