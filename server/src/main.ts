6150695a4b72459707e94288b20e8e2311497609
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-25 16:49:49 +0900

    브라우저에 남아있던 cache때문에 못 불러오는 것이었음

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express'; // NestExpressApplication 추가
-import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule); // NestExpressApplication 사용
-  app.useStaticAssets(path.join(__dirname, '..', '..', 'client', 'build'));
-  console.log(path.join(__dirname, '..', '..', 'client', 'build'));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // CRA 빌드 결과물이 위치한 경로를 지정합니다.
+  console.log(path.join(__dirname, '..', '..', 'client', 'build'))
+  const staticFilesPath = path.join(__dirname, '..', '..', 'client', 'build');
+  app.useStaticAssets(staticFilesPath);
+
+  // '/' 요청이 왔을 때 빌드된 index.html 파일을 반환합니다.
+  app.use('*', (req: Request, res: Response, next: NextFunction) => {
+    res.sendFile(path.join(staticFilesPath, 'index.html'));
+  });
 
   await app.listen(3000);
 }
