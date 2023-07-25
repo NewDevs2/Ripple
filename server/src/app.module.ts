21bd7ef6c33cfcd8db42655dd114111b9783f87d
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-25 17:19:37 +0900

    소켓 Module, GateWay 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,8 +4,9 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { Express as expressMiddleware } from 'express';
 import * as path from 'path';
 // import { StaticFileServeModule } from './static-file-serve/static-file-serve.module';
+import { SocketModule } from './socket/socket.module';
 @Module({
-  imports: [],
+  imports: [SocketModule],
   controllers: [],
   providers: [],
 })
