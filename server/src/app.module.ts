40bd332074cb6dcbf98c789ffe841b828ba0c44b
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-25 16:26:52 +0900

    소켓 서버 따로 사용하지 않고 테스트(요청 두번 옴)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,10 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { Express as expressMiddleware } from 'express';
 import * as path from 'path';
 import { StaticFileServeModule } from './static-file-serve/static-file-serve.module';
-import { SocketGateway } from './socket/socket.gateway';
+// import { SocketGateway } from './socket/socket.gateway';
 @Module({
   imports: [StaticFileServeModule],
   controllers: [],
-  providers: [SocketGateway],
+  providers: [],
 })
 export class AppModule {}
