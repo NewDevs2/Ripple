f06f69664ec18532832b97fb1762dc5d6c96268b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-31 16:03:42 +0900

    serve-static 패키지 설치

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // client/build 폴더 서빙
+  app.useStaticAssets(join(__dirname, '../', 'client', 'build'));
+
   await app.listen(3000);
 }
 bootstrap();
