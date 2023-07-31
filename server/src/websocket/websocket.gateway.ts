5a9752ac0e117b9218242501de878be8d765d3e1
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-31 15:51:43 +0900

    nest.js에서 socket.io를 설치

---
+++ server/src/websocket/websocket.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class WebsocketGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  users = new Map<string, string>();
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('새로운 클라이언트 연결됨 : ', client.id);
+  }
+
+  handleDisconnect(client: any) {
+    console.log('클라이언트 연결 끊김 : ', client.id);
+    this.users.delete(client.id);
+  }
+
+  @SubscribeMessage('location')
+  handleLocation(client: any, data: string) {
+    const userLocationString = `유저 (${client.id}) 위치 정보 : ${data}`;
+    this.users.set(client.id, userLocationString);
+    this.server.emit('location', Array.from(this.users.values()));
+  }
+}
