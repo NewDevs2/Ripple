f170c84e482023e5f013c61baed05858351720ec
gwonyejun <kyjuni1059@gmail.com>
2023-08-04 18:03:49 +0900

    dist 추가 커밋

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,19 +7,5 @@ import { WebsocketGateway } from './websocket/websocket.gateway';
   imports: [],
   controllers: [AppController],
   providers: [AppService, WebsocketGateway],
-=======
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-import { Express as expressMiddleware } from 'express';
-import * as path from 'path';
-// import { StaticFileServeModule } from './static-file-serve/static-file-serve.module';
-import { SocketModule } from './socket/socket.module';
-import { PageModule } from './page/page.module';
-@Module({
-  imports: [SocketModule, PageModule],
-  controllers: [],
-  providers: [],
->>>>>>> kwon5-1
 })
 export class AppModule {}
