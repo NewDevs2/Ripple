d05c39671152ad475f4c81f837bbf1514f09f052
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-07 11:09:42 +0900

    샘플 코드 작성. 작동되지 않음.

--- client/src/musicPlayer.tsx
+++ client/src/musicPlayer.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 
-const Player: React.FC = () => {
+interface YoutubePlayerProps {
+  videoId: string;
+  onReady?: () => void;
+  onStateChange?: (event: YT.OnStateChangeEvent) => void;
+}
+
+const Player: React.FC<YoutubePlayerProps> = ({
+  videoId,
+  onReady,
+  onStateChange,
+}) => {
+  const playerRef = useRef<YT.Player | null>(null);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.youtube.com/iframe_api";
+    script.async = true;
+
+    const onYouTubeIframeAPIReady = () => {
+      playerRef.current = new window.YT.Player("youtube-player", {
+        height: "360",
+        width: "640",
+        videoId,
+        events: {
+          onReady: () => {
+            if (onReady) {
+              onReady();
+            }
+          },
+          onStateChange: (event: YT.OnStateChangeEvent) => {
+            if (onStateChange) {
+              onStateChange(event);
+            }
+          },
+        },
+      });
+    };
+    // 스크립트가 로드되면 onYouTubeIframeAPIReady 함수를 호출하도록 설정
+    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+
+    // 컴포넌트가 언마운트 될 때 스크립트 이벤트 리스너를 정리
+    return () => {
+      delete window.onYouTubeIframeAPIReady;
+    };
+  }, [videoId, onReady, onStateChange]);
+
   return (
     <>
       <div className="App">
@@ -9,6 +54,7 @@ const Player: React.FC = () => {
           <p>
             <code>music player</code>.
           </p>
+          <div id="youtube-player"></div>
         </header>
       </div>
     </>
