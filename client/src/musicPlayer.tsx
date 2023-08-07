592a5d16c5f790e43c6f014df71a4225b27a1c7f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-07 14:14:54 +0900

    react-icons을 설치하여 이전/다음/재생 버튼을 추가함.

--- client/src/musicPlayer.tsx
+++ client/src/musicPlayer.tsx
@@ -3,6 +3,12 @@ import "./App.css";
 
 import YouTube from "react-youtube";
 
+import { FaPlay } from "react-icons/fa";
+import {
+  TbPlayerSkipBackFilled,
+  TbPlayerSkipForwardFilled,
+} from "react-icons/tb";
+
 const Player: React.FC = () => {
   const playlistId = "PL1Qutxw15MZxpTtg1IjV42JFa4pMjMSk5"; // 재생 목록의 ID
 
@@ -25,6 +31,11 @@ const Player: React.FC = () => {
             <code>music player</code>.
           </p>
           <YouTube opts={opts} />
+          <div className="player">
+            <TbPlayerSkipBackFilled />
+            <FaPlay />
+            <TbPlayerSkipForwardFilled />
+          </div>
         </header>
       </div>
     </>
