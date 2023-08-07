9d559a1732452cdb52846782f1c9e314d46ebbc8
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-07 12:55:30 +0900

    react-youtube가 플레이리스트를 추가하지 못하는 이유 찾는 중

--- client/src/musicPlayer.tsx
+++ client/src/musicPlayer.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import YouTube from "react-youtube";
+
 const Player: React.FC = () => {
+  const playlistId = "PL1Qutxw15MZxpTtg1IjV42JFa4pMjMSk5"; // 재생 목록의 ID
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+      listType: "playlist",
+      list: playlistId,
+    },
+  };
+
   return (
     <>
       <div className="App">
@@ -9,6 +23,7 @@ const Player: React.FC = () => {
           <p>
             <code>music player</code>.
           </p>
+          <YouTube {...opts} />
         </header>
       </div>
     </>
