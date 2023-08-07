cc86ecea44626761e8f6407810f3b77eb44bcbf3
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-07 11:23:17 +0900

    react-youtube를 추가하여 우선 유튜브 플레이어를 추가함.

--- client/src/musicPlayer.tsx
+++ client/src/musicPlayer.tsx
@@ -1,51 +1,16 @@
 import React, { useEffect, useRef } from "react";
 import "./App.css";
 
-interface YoutubePlayerProps {
-  videoId: string;
-  onReady?: () => void;
-  onStateChange?: (event: YT.OnStateChangeEvent) => void;
-}
+import YouTube from "react-youtube";
 
-const Player: React.FC<YoutubePlayerProps> = ({
-  videoId,
-  onReady,
-  onStateChange,
-}) => {
-  const playerRef = useRef<YT.Player | null>(null);
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://www.youtube.com/iframe_api";
-    script.async = true;
-
-    const onYouTubeIframeAPIReady = () => {
-      playerRef.current = new window.YT.Player("youtube-player", {
-        height: "360",
-        width: "640",
-        videoId,
-        events: {
-          onReady: () => {
-            if (onReady) {
-              onReady();
-            }
-          },
-          onStateChange: (event: YT.OnStateChangeEvent) => {
-            if (onStateChange) {
-              onStateChange(event);
-            }
-          },
-        },
-      });
-    };
-    // 스크립트가 로드되면 onYouTubeIframeAPIReady 함수를 호출하도록 설정
-    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-
-    // 컴포넌트가 언마운트 될 때 스크립트 이벤트 리스너를 정리
-    return () => {
-      delete window.onYouTubeIframeAPIReady;
-    };
-  }, [videoId, onReady, onStateChange]);
+const Player: React.FC = () => {
+  const opts = {
+    height: "100%",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
     <>
@@ -54,7 +19,13 @@ const Player: React.FC<YoutubePlayerProps> = ({
           <p>
             <code>music player</code>.
           </p>
-          <div id="youtube-player"></div>
+          <YouTube
+            id="youtube-player"
+            className="player"
+            style={{ width: "100%", height: "100%" }}
+            videoId={"aUndbCBVV0c"}
+            opts={opts}
+          />
         </header>
       </div>
     </>
