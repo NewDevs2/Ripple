d87d3ee8ab2a9b4393561fd098edf03eb1a0f2ba
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-07 11:52:21 +0900

    라이브러리를 사용하지 않고 youtube iframe api를 사용하기 위해 다시 초기화

--- client/src/musicPlayer.tsx
+++ client/src/musicPlayer.tsx
@@ -1,17 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./App.css";
 
-import YouTube from "react-youtube";
-
 const Player: React.FC = () => {
-  const opts = {
-    height: "100%",
-    width: "100%",
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-
   return (
     <>
       <div className="App">
@@ -19,13 +9,6 @@ const Player: React.FC = () => {
           <p>
             <code>music player</code>.
           </p>
-          <YouTube
-            id="youtube-player"
-            className="player"
-            style={{ width: "100%", height: "100%" }}
-            videoId={"aUndbCBVV0c"}
-            opts={opts}
-          />
         </header>
       </div>
     </>
