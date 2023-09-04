2195b922a09bb265a7e43eddd164eed03f315d13
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-04 22:14:32 +0900

    클라이언트측 콘솔에서 responseData확인 안되던 문제 -> 코드는 수정된게 없는데 해결

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -6,6 +6,6 @@ export class AuthController {
   @Get('kakao') // '/auth/kakao'를 'kakao'로 변경
   kakaoAuth(@Res() res: Response) {
     console.log('유저가 카카오 로그인 요청');
-    res.send('하이');
+    res.json({ message: '카카오 로그인 요청' });
   }
 }
