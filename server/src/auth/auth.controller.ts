2bc2e18d15d54de1491e50b5feb1d71f2ffbdf5d
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-04 22:58:37 +0900

    최초 요청은 서버에서 하지 않고 클라이언트에서 하려함

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   @Get('kakao') // '/auth/kakao'를 'kakao'로 변경
-  kakaoAuth(@Res() res: Response) {
-    console.log('유저가 카카오 로그인 요청');
-    res.json({ message: '카카오 로그인 요청' });
+  async kakaoLogin(@Res() res: Response, @Req() req: Request) {
+    // 카카오 인증 요청
+    const REST_API_KEY = '9da388ef82d7b351156a7e2e0772c757';
+    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=$9da388ef82d7b351156a7e2e0772c757&redirect_uri=http://localhost:3000&response_type=code`;
+
+    return res.redirect(kakaoAuthUrl);
   }
 }
