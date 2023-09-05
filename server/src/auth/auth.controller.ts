a90df21b396937af2bd9461af35ad13ba1cbd515
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-05 16:41:52 +0900

    카카오측 code데이터 받아오기 확인

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -6,9 +6,7 @@ export class AuthController {
   @Get('kakao') // '/auth/kakao'를 'kakao'로 변경
   async kakaoLogin(@Res() res: Response, @Req() req: Request) {
     // 카카오 인증 요청
-    const REST_API_KEY = '9da388ef82d7b351156a7e2e0772c757';
-    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=$9da388ef82d7b351156a7e2e0772c757&redirect_uri=http://localhost:3000&response_type=code`;
-
-    return res.redirect(kakaoAuthUrl);
+    const path = `https://kauth.kakao.com/oauth/authorize?client_id=9da388ef82d7b351156a7e2e0772c757&redirect_uri=http://localhost:3000/auth/kakao&response_type=code`;
+    return res.redirect(path);
   }
 }
