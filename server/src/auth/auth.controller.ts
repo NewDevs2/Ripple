40b9762e49f29757193e1bd8e1ebd648b2aa68b6
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-06 21:13:31 +0900

    요청도 안가고 코드 데이터가 추출이 안됨

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-@Controller('oauth')
+@Controller('auth')
 export class AuthController {
-  @Get('kakao') // '/auth/kakao'를 'kakao'로 변경
+  @Get('/kakao') // '/auth/kakao'를 'kakao'로 변경
   async kakaoLogin(@Res() res: Response, @Req() req: Request) {
     // 카카오 인증 요청
-    return res.send({ message: '요청은 잘 옴' });
+    return res.redirect('/');
   }
 }
