f0c28681dae2d258547a7c73627fdcad1cef6ecc
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-08 14:30:16 +0900

    dotenv를 활용해 api키와 redirect URI 숨김

--- client/src/kakaoLogin.tsx
+++ client/src/kakaoLogin.tsx
@@ -1,6 +1,7 @@
 import kakao_login from './kakao_login.png';
+
 const Kakao = () : JSX.Element => {
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=e13169ec36f699296518921c6bd1fbd7&redirect_uri=http://localhost:3000/oauth/kakao&response_type=code`
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_kakao_client_id}&redirect_uri=${process.env.REACT_APP_kakao_restAPI_key}&response_type=code`
 function request_kakao() {
   window.location.href = kakaoURL;
 }
