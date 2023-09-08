843e827a460d4edb4c54968ec36896e37bc9e259
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-08 15:11:22 +0900

    카카오 서버에서 인가 코드(access token) 받아옴

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Req, Res } from '@nestjs/common';
+import axios from 'axios';
 import { Request, Response } from 'express';
-
 @Controller('oauth')
 export class AuthController {
   @Post('kakao') // POST 요청을 받음
@@ -12,8 +12,24 @@ export class AuthController {
       if (code) {
         // 필요한 작업을 수행합니다.
         console.log('인가 코드:', code);
+        axios
+          .post('https://kauth.kakao.com/oauth/token', null, {
+            params: {
+              grant_type: 'authorization_code',
+              client_id: 'e13169ec36f699296518921c6bd1fbd7',
+              redirect_uri: 'http://localhost:3000/oauth/kakao',
+              code: code,
+            },
+          })
+          .then((response) => {
+            console.log(
+              `카카오에서 받아온 데이터 : ${response.data.access_token}`,
+            );
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
-        // 예: 서버로 code를 전송하여 액세스 토큰을 요청하거나 추가 작업을 수행합니다.
         return res.send('요청왔습니다.');
       } else {
         console.log('요청에 인가 코드가 없습니다.');
