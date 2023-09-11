e3abf49160ab4be7d53586c6866598185d03804f
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-11 19:18:08 +0900

    유저의 정보를 받아온 함수에서, 콜백함수 실행 -> db에 유저 정보 저장

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,7 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import mongoose from 'mongoose';
+
 @Injectable()
 export class UserService {
+  async createUserInformation(user_info: any) {
+    try {
+      //mongoDB 연결
+      mongoose.connect('mongodb://localhost/riple', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as mongoose.ConnectOptions);
+      //인터페이스
+      interface IUser extends Document {
+        id: number;
+      }
+      // 새로운 스키마 생성
+      const userSchema = new mongoose.Schema<IUser>({
+        id: Number,
+      });
+      // 유저 모델 생성
+      const User = mongoose.model<IUser>('User', userSchema);
+      const saveUserInfo = new User({ id: user_info.id });
+      await saveUserInfo.save();
+    } catch (error) {
+      console.log(`DB저장 실패 : ${error}`);
+    }
+  }
   async callUserControllerMethod(ACCESS_TOKEN: any) {
     try {
       console.log(`토큰 데이터 : ${ACCESS_TOKEN}`);
@@ -13,7 +38,9 @@ export class UserService {
       if (response.status === 200) {
         const userInfo = response.data;
         console.log('사용자 정보:', userInfo);
-        return userInfo;
+
+        //받아온 유저 정보를 database에 저장할 콜백함수 실행
+        return this.createUserInformation(userInfo);
       } else {
         console.error('사용자 정보를 가져오는 데 실패했습니다.');
         return null;
