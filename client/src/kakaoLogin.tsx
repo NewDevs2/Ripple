f433183b79c19c0adcb74443c080fc4a836743ba
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-10-11 19:49:57 +0900

    쓰레기 코드 제거 및 수동 로그인, 로그아웃 상태 관리 구현 완료

--- client/src/kakaoLogin.tsx
+++ client/src/kakaoLogin.tsx
@@ -1,41 +1,38 @@
-import kakao_login from './kakao_login.png';
-// 로그인 상태를 구독
 import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
+import kakao_login from './kakao_login.png';
+// 로그인 상태 값
 import { isLoggedInState, userInformationState } from './auth/loginState';
+// 로그인 상태 관리 함수
+import { useSetLoggedInState, useSetUserInformation } from './auth/loginState';
 const Kakao = () : JSX.Element => {
-  // 로그인 상태 관리
+  // 로그인 상태 값
   const isLoggedin = useRecoilValue(isLoggedInState);
   const userInformation = useRecoilValue(userInformationState);
-  // 토큰 상태 관리
+  // 로그인 상태 관리 함수
+  const setLoggedInState = useSetLoggedInState();
+  const setUserInformation = useSetUserInformation();
+
   // 로그인 때 이동 할 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_kakao_client_id}&redirect_uri=${process.env.REACT_APP_kakao_restAPI_key}&response_type=code`
-  // 쿠키
-  // const [cookies,,removeCookie] = useCookies(['kakao_token']);
-  // useEffect(()=> {
-  //   // 만약 카카오 토큰이 있으면 컴포넌트 재실행
-  //   if(cookies.kakao_token) {
-  //     console.log(`카카오 토큰 있고 컴포넌트 재실행 됨${cookies.kakao_token}`);
-  //     setIsLoggedIn(true);
-  //   }
-  // },[cookies, setIsLoggedIn])
+  
+  // 로그인
   const kakaoLogIn = () => {
-    // 로그인
     window.location.href = kakaoURL;
   }
-
-  // const kakaoLogOut = () => {
-    // 쿠키 삭제
-    // removeCookie('kakao_token');
-    // 로그아웃
-  //   setIsLoggedIn(false);
-  // }
+  // 로그아웃
+  const kakaoLogOut = () => {
+    // 로그인 상태, 유저 정보 초기화
+    setLoggedInState(false);
+    setUserInformation(null);
+  }
   useEffect(() => {
   console.log(isLoggedin);
   console.log(userInformation);
   }, [isLoggedin, userInformation]);
   if (isLoggedin) {
-    return <div>{userInformation}님 안녕하세요.</div>
+    return <><div>{userInformation}님 안녕하세요.</div>
+    <button onClick={kakaoLogOut}>로그아웃</button></>
   }
   return (
         <div onClick={kakaoLogIn}>
